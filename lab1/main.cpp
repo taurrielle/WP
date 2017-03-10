de383df90031a8a73bc2b402d8c49c695e3e608d
taurrielle <irene.ungureanu96@gmail.com>
2017-03-10 12:46:14 +0200

    fixed change font button

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -25,7 +25,7 @@ void checkIfCompOpened()
     {
         MessageBox(NULL, "Wow! You're not wasting time on the Internet. \nWell, that's a first! ", "Amazing", MB_OK |MB_ICONINFORMATION);
     }
-    else
+     else
     {
         MessageBox(NULL, "Stop browsing the Internet and get to work!!!", "Get to work", MB_OK | MB_ICONERROR);
     }
@@ -162,7 +162,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                                hwnd, (HMENU)ID_TEXT2, GetModuleHandle(NULL), NULL);
 
         text3 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), TEXT(""),
-                               WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_READONLY,
+                               WS_VISIBLE | WS_CHILD | ES_MULTILINE |ES_READONLY,
                                0, 0, 0, 0,
                                hwnd, (HMENU)ID_TEXT3, GetModuleHandle(NULL), NULL);
         SendMessage(comboBox, CB_ADDSTRING, 0, (LPARAM)"Select color");
@@ -171,8 +171,11 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         SendMessage(comboBox, CB_ADDSTRING, 0, (LPARAM)"Pink");
         SendMessage(comboBox, CB_ADDSTRING, 0, (LPARAM)"Pick random");
         SendMessage(comboBox, CB_SETCURSEL, 0, 0);
-        //hfFont = CreateFont(20,0,0,0,0,0,0,0,0,0,0,0,0,"Helvetica");
-        //SendMessage(button1, WM_SETFONT, (WPARAM)hfFont, MAKELPARAM(FALSE,0));
+
+        hfFont = CreateFont(16,0,0,0,0,0,0,0,0,0,0,0,0,"Courier New");
+        SendMessage(button3, WM_SETFONT, (WPARAM)hfFont, MAKELPARAM(FALSE,0));
+        SendMessage(text3, WM_SETFONT, (WPARAM)hfFont, MAKELPARAM(FALSE,0));
+
         break;
     }
 
@@ -199,30 +202,35 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                 if(comboIndex == 0)
                 {
                     defaultFlag = true;
+                    redFlag = textFlag = purpleFlag = pinkFlag = false;
                     InvalidateRect(text3, NULL, TRUE);
                     break;
                 }
                 else if(comboIndex == 1)
                 {
                     redFlag = true;
+                    textFlag = defaultFlag = purpleFlag = pinkFlag = false;
                     InvalidateRect(text3, NULL, TRUE);
                     break;
                 }
                 else if(comboIndex == 2)
                 {
                     purpleFlag = true;
+                    redFlag = defaultFlag = textFlag = pinkFlag = false;
                     InvalidateRect(text3, NULL, TRUE);
                     break;
                 }
                 else if(comboIndex == 3)
                 {
                     pinkFlag = true;
+                    redFlag = defaultFlag = textFlag = purpleFlag = false;
                     InvalidateRect(text3, NULL, TRUE);
                     break;
                 }
                 else if(comboIndex == 4)
                 {
                     textFlag = true;
+                    redFlag = defaultFlag = purpleFlag = pinkFlag = false;
                     InvalidateRect(text3, NULL, TRUE);
                     break;
                 }
@@ -268,7 +276,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             int index;
             index = rand() % 9;
             hfFont = CreateFont(0,0,0,0,0,0,0,0,0,0,0,0,0,fontList[index]);
-            SendMessage(text3, WM_SETFONT, (WPARAM)hfFont, 1);
+            SendMessage(text3, WM_SETFONT, (WPARAM)hfFont, MAKELPARAM(FALSE,0));
             InvalidateRect(text3, NULL, TRUE);
             break;
         }
@@ -296,41 +304,36 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     {
         if(defaultFlag == true && (HWND)lParam == text3)
         {
-            defaultFlag = false;
             HBRUSH hbr = (HBRUSH) DefWindowProc(hwnd, message, wParam, lParam);
             SetTextColor((HDC) wParam, RGB(0,0,0));
             return (BOOL) hbr;
         }
 
-        if(textFlag == true && (HWND)lParam == text3)
+        else if(textFlag == true && (HWND)lParam == text3)
         {
-            textFlag = false;
             HBRUSH hbr = (HBRUSH) DefWindowProc(hwnd, message, wParam, lParam);
             SetTextColor((HDC) wParam, RGB(rand() % 256, rand() % 256, rand() % 256));
             return (BOOL) hbr;
         }
 
 
-        if(redFlag == true && (HWND)lParam == text3)
+        else if(redFlag == true && (HWND)lParam == text3)
         {
-            redFlag = false;
             HBRUSH hbr = (HBRUSH) DefWindowProc(hwnd, message, wParam, lParam);
             SetTextColor((HDC) wParam, RGB(153, 0, 0));
             return (BOOL) hbr;
         }
 
-        if(purpleFlag == true && (HWND)lParam == text3)
+        else if(purpleFlag == true && (HWND)lParam == text3)
         {
-            purpleFlag = false;
             HBRUSH hbr = (HBRUSH) DefWindowProc(hwnd, message, wParam, lParam);
             SetTextColor((HDC) wParam, RGB(153, 115, 255));
             return (BOOL) hbr;
         }
 
 
-        if(pinkFlag == true && (HWND)lParam == text3)
+        else if(pinkFlag == true && (HWND)lParam == text3)
         {
-            pinkFlag = false;
             HBRUSH hbr = (HBRUSH) DefWindowProc(hwnd, message, wParam, lParam);
             SetTextColor((HDC) wParam, RGB(255, 179, 217));
             return (BOOL) hbr;
@@ -348,11 +351,14 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             }
             else
             {
-                SetTextColor(hdc, RGB(110,123,139));                        // Text color
+                SetTextColor(hdc, RGB(165, 164, 164));                        // Text color
             }
+            hdc = (HDC) wParam;
+            SetBkColor(hdc, RGB(255, 237, 237));
+            return (INT_PTR)CreateSolidBrush(RGB(255, 237, 237));
         }
 
-        if(GetDlgCtrlID((HWND)lParam) == ID_TEXT2)
+        else if(GetDlgCtrlID((HWND)lParam) == ID_TEXT2)
         {
             hdc = (HDC)wParam;                                              //Get handles
             if(flag2)
@@ -361,11 +367,55 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             }
             else
             {
-                SetTextColor(hdc, RGB(110,123,139));                        // Text color
+                SetTextColor(hdc, RGB(165, 164, 164));                        // Text color
             }
+            hdc = (HDC) wParam;
+            SetBkColor(hdc, RGB(255, 237, 237));
+            return (INT_PTR)CreateSolidBrush(RGB(255, 237, 237));
         }
         break;
+    /*
+      case WM_CTLCOLOREDIT:
+                if(GetDlgCtrlID((HWND)lParam) == ID_BUTTON)
+                {
+                    hdc = (HDC)wParam;                                              //Get handles
+                        color = CreateSolidBrush(RGB(0, 0, 255));
+                        SetTextColor(hdc, RGB(0,0,0));                              // Text color
+                        SetBkMode(hdc, TRANSPARENT);                                // EditBox Backround Mode
+                        SetBkColor(hdc,(LONG)color);                                // Backround color for EditBox
 
+                    return (LONG)color;                                             // Paint it
+                }
+                break;
+
+                 case WM_DRAWITEM:
+                if ((UINT)wParam == ID_BUTTON) {
+                    LPDRAWITEMSTRUCT lpdis = (DRAWITEMSTRUCT*)lParam;
+                    SIZE size;
+                    char szLondonBtnText[7] = "bla";
+                    GetTextExtentPoint32(lpdis->hDC, szLondonBtnText, strlen(szLondonBtnText), &size);
+                    SetTextColor(lpdis->hDC, RGB(245,245,220));
+                    SetBkColor(lpdis->hDC, RGB(150,104,108));
+
+                    ExtTextOut(
+                        lpdis->hDC,
+                        ((lpdis->rcItem.right - lpdis->rcItem.left) - size.cx) / 2,
+                        ((lpdis->rcItem.bottom - lpdis->rcItem.top) - size.cy) / 2,
+                        ETO_OPAQUE | ETO_CLIPPED,
+                        &lpdis->rcItem,
+                        szLondonBtnText,
+                        strlen(szLondonBtnText),
+                        NULL);
+
+                    DrawEdge(
+                        lpdis->hDC,
+                        &lpdis->rcItem,
+                        (lpdis->itemState & ODS_SELECTED ? EDGE_SUNKEN : EDGE_RAISED ),
+                        BF_RECT);
+                    return TRUE;
+    			}
+
+    */
     case WM_GETMINMAXINFO:
     {
         LPMINMAXINFO winSize = (LPMINMAXINFO)lParam;
@@ -382,17 +432,20 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         {
         case SC_MINIMIZE:
         {
-            checkIfCompOpened();
+            RECT rc;
+
+            GetWindowRect ( hwnd, &rc ) ;
+
+            int xPos = rand() % 800;
+            int yPos = rand() % 800;
+
+            SetWindowPos( hwnd, 0, xPos, yPos, 0, 0, SWP_NOZORDER | SWP_NOSIZE );
             break;
         }
 
         case SC_MAXIMIZE:
         {
-            RECT rc;
-            GetWindowRect ( hwnd, &rc ) ;
-            int xPos = rand() % 800;
-            int yPos = rand() % 800;
-            SetWindowPos( hwnd, 0, xPos, yPos, 0, 0, SWP_NOZORDER | SWP_NOSIZE );
+            checkIfCompOpened();
             break;
         }
 
@@ -419,3 +472,4 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     }
     return 0;
 }
+
